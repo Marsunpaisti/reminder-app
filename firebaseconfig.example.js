e51646c9cd76d17f623e5217e5bff7b5ee7d6a49
Marsunpaisti <marsunpaisti@gmail.com>
2020-12-15 11:52:01 +0200

    Created a new Expo app

---
+++ firebaseconfig.example.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+	apiKey: "api-key",
+	authDomain: "example.firebaseapp.com",
+	databaseURL: "https://example.firebaseio.com",
+	projectId: "project-id",
+	storageBucket: "project-id.appspot.com",
+	messagingSenderId: "sender-id",
+	appId: "app-id",
+	measurementId: "G-measurement-id",
+};
+
+export default firebaseConfig;
